78ca979708319844a6eaf3f4b6051e3004500cdb
liupanwang <liupanwang@ypshengxian.com>
2020-02-01 12:08:31 +0800

    修改首页的跳转路由

--- src/utils/sdkApi.ts
+++ src/utils/sdkApi.ts
@@ -114,7 +114,7 @@ export const share = (url: string, img: string, title: string) => {
 }
 
 const appRouter: Object = {
-  '/pages/index/index': 'yp://nativeLogin',                       // 首页
+  '/pages/index/index': 'yp://nativeGoHome',                      // 首页
   '/pages/shoppingCart/main': 'yp://nativeShoppingCart',          // 购物车
   '/pages/detail/main': 'yp://nativeGoodsPage',                   // 商品详情页
   '/pages/sort/main': 'yp://nativeGoCategory',                    // 分类页
